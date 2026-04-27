9abda640e7dbf7f5da0898f5c03595343671e911
sachinNishalka <nishalkasachin@gmail.com>
2026-04-27 14:17:12 +0530

    implemented feature for view member

--- src/main/java/pro/sachin/fity/sercives/impl/MemberServiceImpl.java
+++ src/main/java/pro/sachin/fity/sercives/impl/MemberServiceImpl.java
@@ -111,4 +111,11 @@ public class MemberServiceImpl implements MemberService {
 
     }
 
+    @Override
+    public Member getMemberById(Long id) {
+        
+        Member member = memberRepository.findById(id).orElseThrow(() -> new RuntimeException("Member not found with id: " + id));
+        return member;
+    }
+
 }
